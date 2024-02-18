d51240b30ba004126e341f4786ed1e09f32b0b53
lescahub <160023663+lescahub@users.noreply.github.com>
2024-02-18 20:59:16 +0700

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// Toggle class active untuk hamburger menu
-const navbarNav = document.querySelector('.navbar-nav');
-// ketika hamburger menu di klik
-document.querySelector('#menu').onclick = () => {
-  navbarNav.classList.toggle('active');
-};
-
